d9de2ae60545f6cd328aa215234fd6b6fb38278e
Chris Feldmann <cfeldmann@gmail.com>
2015-09-25 17:14:34 -0400

    remove video, cookie setting

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 $(document).ready(function(){
 
-	setTimeout(function(){
-		$('.lightbox').addClass('show');
-	}, 2000);
+	if( readCookie( 'forest-trailer') !== "true"){
+		createCookie( 'forest-trailer', true, 1);
+
+		setTimeout(function(){
+			showVideo();
+		}, 2000);
+	}
+
+	
 	
 	$('.show-video').on({
 		mousedown : function(){
-			$('.lightbox').addClass('show');
+			showVideo()
+
 		}
 	});
 
@@ -15,6 +22,42 @@ $(document).ready(function(){
 			e.preventDefault();
 			e.stopPropagation();
 			$('.lightbox').removeClass('show');
+			setTimeout( function(){
+				
+				$('.videowrapper iframe').remove();
+				
+			}, 300);
 		}
 	});
 });
+
+function showVideo(){
+	var markup = '<iframe  src="https://www.youtube.com/embed/odkf4SAY8pI?autoplay=1" frameborder="0" allowfullscreen></iframe>';
+	$('.videowrapper').append( markup );
+	$('.lightbox').addClass('show');
+}
+
+function createCookie(name,value,days) {
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime()+(days*24*60*60*1000));
+		var expires = "; expires="+date.toGMTString();
+	}
+	else var expires = "";
+	document.cookie = name+"="+value+expires+"; path=/";
+}
+
+function readCookie(name) {
+	var nameEQ = name + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0;i < ca.length;i++) {
+		var c = ca[i];
+		while (c.charAt(0)==' ') c = c.substring(1,c.length);
+		if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+	}
+	return null;
+}
+
+function eraseCookie(name) {
+	createCookie(name,"",-1);
+}
